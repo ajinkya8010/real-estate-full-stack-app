f21cf574321ba38507d22d9ff7e663b0ed543cb7
ajinkya8010 <ajinkya8975@gmail.comcom>
2024-09-01 21:38:22 +0530

    -

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://real-estate-backend-620y.onrender.com",
   withCredentials: true,
 });
 
