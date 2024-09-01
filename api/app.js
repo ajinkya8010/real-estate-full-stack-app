c98dee1995abac21abf0d22f1c9eef7fcf8c8767
ajinkya8010 <ajinkya8975@gmail.comcom>
2024-09-01 22:26:22 +0530

    -

--- api/app.js
+++ api/app.js
@@ -15,10 +15,10 @@ app.use(cors({ origin: clientUrl, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/test", testRoute);
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
+app.use("/posts", postRoute);
+app.use("/test", testRoute);
 
 
 
--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://real-estate-backend-j0j1.onrender.com/api",
+  baseURL: "https://real-estate-backend-j0j1.onrender.com",
   withCredentials: true,
 });
 
