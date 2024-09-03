a0dcbc22e97366700cb0b20506475c0811d4486a
ajinkya8010 <ajinkya8975@gmail.com>
2024-09-04 01:01:41 +0530

    images-in-production

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -5,9 +5,9 @@ import Pin from "../pin/Pin";
 import L from 'leaflet';
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/marker-icon-2x.png',
-  iconUrl: '/marker-icon.png',
-  shadowUrl: '/marker-shadow.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: '/https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 function Map({ items }) {
   return (
