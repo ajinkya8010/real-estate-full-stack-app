d8acbc76423b10ddbdf3749d2fcb5896481d3ebc
ajinkya8010 <ajinkya8975@gmail.com>
2024-09-03 13:18:33 +0530

    mapchanges

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 function Pin({ item }) {
   return (
-    <Marker position={[item.latitude, item.longitude]} icon={customIcon} >
+    <Marker position={[item.latitude, item.longitude]}>
       <Popup>
         <div className="popupContainer">
           <img src={item.images[0]} alt="" />
