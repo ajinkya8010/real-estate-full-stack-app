6d7fa0434d7fd3b2a33c88ef294e52b13c3cfadf
ajinkya8010 <ajinkya8975@gmail.comcom>
2024-09-14 20:02:15 +0530

    icon-changed

--- client/src/lib/loaders.js
+++ client/src/lib/loaders.js
@@ -7,7 +7,7 @@ export const singlePageLoader = async ({ request, params }) => {
 };
 export const listPageLoader = async ({ request, params }) => {
   const query = request.url.split("?")[1];
-  const postPromise = apiRequest("/posts?" + query);
+  const postPromise = await apiRequest("/posts?" + query);
   console.log("API Response:", postPromise.data);
   return defer({
     postResponse: postPromise,
